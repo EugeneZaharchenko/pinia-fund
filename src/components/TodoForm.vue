65b184f9cfac6c4c27e3834a5c1006a7d605f901
eugene_zaharch <eugenezaharchenko@gmail.com>
2024-05-13 07:48:56 +0300

    store add action

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,11 +1,26 @@
 <template>
     <div>
-
+        <form @submit.prevent="addItemAndClear(todo)">
+            <input v-model="todo" type="text">
+            <button>Add</button>
+        </form>
     </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import { useTodoListStore } from '@/stores/todoList'
 
+const todo = ref('')
+const store = useTodoListStore()
+
+function addItemAndClear(item) {
+    if (item.length === 0) {
+        return
+    }
+    store.addTodo(item)
+    todo.value = ''
+}
 </script>
 
 <style scoped>
--- src/stores/todoList.js
+++ src/stores/todoList.js
@@ -4,5 +4,15 @@ import TodoList from "../components/TodoList.vue"
 export default useTodoListStore = defineStore('todoList', {
     state: () => ({
         todoList: []
-    })
+    }),
+    actions: {
+        addTodo(item) {
+            this.todoList.push({item, id: this.id++, comleted: false})
+        },
+        deleteTodo(itemId) {
+            this.todoList = this.todoList.filter((object) => {
+                return object.id !== itemId
+            })
+        }
+    }
 })
